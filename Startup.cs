11ee136f6d8b7f4dc4e7cb5faf505cd87726a871
burman2005 <burman2005@gmail.com>
2020-04-11 13:51:39 +0530

    commit code

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Text;
 using CoreUIAppn.Models;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -10,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 
 namespace CoreUIAppn
 {
@@ -29,14 +25,14 @@ namespace CoreUIAppn
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            //services.AddMvc(option => option.EnableEndpointRouting = false)
-            //    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
-            //    .ConfigureApiBehaviorOptions(o => { o.SuppressModelStateInvalidFilter = true; })
-            //    .AddJsonOptions(options =>
-            //    {
-            //        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-            //        options.JsonSerializerOptions.PropertyNamingPolicy = null;
-            //    });
+            services.AddMvc(option => option.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .ConfigureApiBehaviorOptions(o => { o.SuppressModelStateInvalidFilter = true; })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
 
             //Add framework services
             services.AddDbContext<MeetingContext>(options =>
@@ -46,21 +42,7 @@ namespace CoreUIAppn
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
 
             app.UseEndpoints(endpoints =>
             {
@@ -74,6 +56,20 @@ namespace CoreUIAppn
                     ctx.Response.ContentLength = 0;
                 }
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
         }
     }
 }
